11bedacbe400769575e1e0e195d1bcdb9074d543
SasuMC <101135746+SasuMC@users.noreply.github.com>
2024-09-03 22:42:57 -0300

    Multiplayer fix

--- src/client/java/dev/cape/mixin/client/PlayerListEntryMixin.java
+++ src/client/java/dev/cape/mixin/client/PlayerListEntryMixin.java
@@ -1,7 +1,10 @@
 package dev.cape.mixin.client;
 
+import com.mojang.authlib.GameProfile;
 import com.mojang.authlib.minecraft.MinecraftProfileTexture;
 import dev.cape.CapeSelector;
+import dev.cape.CapeSelectorClient;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Final;
@@ -18,8 +21,13 @@ public abstract class PlayerListEntryMixin {
 
 	@Shadow @Final private Map<MinecraftProfileTexture.Type, Identifier> textures;
 
+	@Shadow @Final private GameProfile profile;
+
 	@Inject(at = @At("HEAD"), method = "getCapeTexture")
 	private void getCapeTexture$0(CallbackInfoReturnable<Identifier> cir) {
+		GameProfile playerProfile = MinecraftClient.getInstance().getSession().getProfile();
+		if (!this.profile.getName().equals(MinecraftClient.getInstance().getSession().getProfile().getName())) return;
+
 		if (CapeSelector.capeSelectedID != null) this.textures.put(MinecraftProfileTexture.Type.CAPE, new Identifier(CapeSelector.MOD_ID, "capes/" + CapeSelector.capeSelectedID + ".png"));
 		else this.textures.put(MinecraftProfileTexture.Type.CAPE, null);
 	}
